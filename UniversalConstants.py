a29fd6740a67e7b7636ecf54f366409dce876ef0
Madr92 <56043554+Madr92@users.noreply.github.com>
2019-12-19 13:49:26 +0100

    Create UniversalConstants.py

---
+++ UniversalConstants.py
@@ -0,0 +1,18 @@
+#this lists the universal constants required for the calculations
+#units are given as comment after constant
+
+TK = 273.15                                             #to convert from degrees C to absolute Temperture
+e=2.71828182845904523536028747135266249775724709369995  #eulers number
+Vm0=22.414                                              #L/mol at standard pressure and standard temperature (0C)
+Vm=24.465                                               #L/mol at 25 C
+bar2atmp=0.9807                                         #relation of bar in atm
+hPa2atmp=0.00098692326671601
+pairO2=0.2095                                          #oxygen content in air
+
+#molar masses for certain common and selected atoms
+mC=12.0107	
+mO=15.9994
+mN=14.0067
+mP=30.9738
+mH=1.0079
+mS=32.065
